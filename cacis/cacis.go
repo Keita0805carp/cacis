0621289d209ea42a00f517c0f244688acb3c5f62
keita0805carp <b1816123@planet.kanazawa-it.ac.jp>
2021-07-31 18:05:25 +0000

    pushもれ

---
+++ cacis/cacis.go
@@ -0,0 +1,78 @@
+package cacis
+
+import (
+  "fmt"
+  "encoding/json"
+  "encoding/binary"
+)
+
+type CacisLayer struct {
+  Type    uint8
+  Length  uint64
+  Payload []byte
+}
+
+const CacisLayerSize = 1 + 8
+
+func (c *CacisLayer) Marshal() []byte {
+  buf := make([]byte, CacisLayerSize)
+  buf[0]   = byte(c.Type)
+  binary.BigEndian.PutUint64(buf[1:], c.Length)
+  buf      = append(buf, c.Payload...)
+  return buf
+}
+
+func Unmarshal(buf []byte) CacisLayer {
+  var c CacisLayer
+  c.Type   = uint8(buf[0])
+  c.Length = binary.BigEndian.Uint64(buf[1:9])
+  c.Payload = buf[9:]
+  return c
+}
+
+func NewCacisPacket(cacisType uint8, l uint64, p []byte) CacisLayer {
+  return CacisLayer{
+    Type:    cacisType,
+    Length:  l,
+    Payload: []byte(p),
+  }
+}
+
+// Request
+func RequestComponentsList() CacisLayer {
+  return NewCacisPacket(1, 0, nil)
+}
+
+func RequestImage() CacisLayer {
+  return NewCacisPacket(2, 0, nil)
+}
+
+// Notify
+func NotifyComponentsListSize(list map[string]string) CacisLayer {
+  p, err := json.Marshal(list)
+  Error(err)
+  return NewCacisPacket(11, uint64(len(p)), nil)
+}
+
+func NotifyImageSize(p []byte) CacisLayer {
+  return NewCacisPacket(12, uint64(len(p)), nil)
+}
+
+// Send
+func SendComponentsList(list map[string]string) CacisLayer {
+  p, err := json.Marshal(list)
+  Error(err)
+  return NewCacisPacket(21, uint64(len(p)), p)
+}
+
+func SendImage(p []byte) CacisLayer {
+  return NewCacisPacket(22, uint64(len(p)), p)
+}
+
+
+func Error(err error) {
+  if err != nil {
+    fmt.Println(err)
+  }
+}
+
