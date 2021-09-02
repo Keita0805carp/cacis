c693bcd3f2177386ffd9398e2067641e589bf5c1
Keita0805carp <b1816123@planet.kanazawa-it.ac.jp>
2021-09-02 18:11:49 +0000

    add simple dhcp

---
+++ connection/dhcp.go
@@ -0,0 +1,39 @@
+package connection
+
+import (
+  "github.com/keita0805carp/cacis/cacis"
+
+  "github.com/coredhcp/coredhcp/server"
+  "github.com/coredhcp/coredhcp/config"
+  "github.com/coredhcp/coredhcp/plugins"
+  pl_leasetime "github.com/coredhcp/coredhcp/plugins/leasetime"
+  pl_netmask "github.com/coredhcp/coredhcp/plugins/netmask"
+  pl_range "github.com/coredhcp/coredhcp/plugins/range"
+  pl_router "github.com/coredhcp/coredhcp/plugins/router"
+  pl_serverid "github.com/coredhcp/coredhcp/plugins/serverid"
+)
+
+const configPath = "./connection/dhcp.conf"
+
+var desiredPlugins = []*plugins.Plugin{
+  &pl_leasetime.Plugin,
+  &pl_netmask.Plugin,
+  &pl_range.Plugin,
+  &pl_router.Plugin,
+  &pl_serverid.Plugin,
+}
+
+func DHCP() {
+	config, err := config.Load(configPath)
+  cacis.Error(err)
+	// register plugins
+  for _, plugin := range desiredPlugins {
+    err := plugins.RegisterPlugin(plugin)
+    cacis.Error(err)
+  }
+
+  srv, err := server.Start(config)
+  cacis.Error(err)
+  err = srv.Wait()
+  cacis.Error(err)
+}
